d94d3c70b4fb3d2367b085a8162304545a493079
Greeley Dawson <hgd4th@Slim.local>
2014-06-11 21:36:34 -0400

    Added Prince variable and function. Added comments

--- hw.js
+++ hw.js
@@ -1,9 +1,28 @@
+// Declaring everyone's age
+
 var greeleyAge = new Date().getFullYear() - 1988;
 var mcLovinAge = new Date().getFullYear() - 1996;
+var princeAge = new Date().getFullYear() - 1958;
+
+//Deciding if everyone gets to drink
 
 if (mcLovinAge < 21) {
-	console.log('Get outta my bar!');
+	console.log('Get outta my bar, McLovin!');
 }
 else {
-	console.log('What can I get you to drink?');
+	console.log('What can I get you to drink, McLovin?');
+}
+
+if (greeleyAge < 21) {
+	console.log('Get outta my bar, Greeley!');
+}
+else {
+	console.log('What can I get you to drink, Greeley?');
+}
+
+if (princeAge < 21) {
+	console.log('Get outta my bar, Prince!');
+}
+else {
+	console.log('What can I get you to drink, Prince?');
 }
