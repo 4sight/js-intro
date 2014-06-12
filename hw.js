8a03bfd8023a1ea4c7da34718f1e48b64f72b9b0
Greeley Dawson <hgd4th@Slim.local>
2014-06-11 21:20:06 -0400

    Bartender's reaction to McLovin

--- hw.js
+++ hw.js
@@ -1,9 +1,9 @@
-var mcLovinAge = 17;
-var greeleyAge = 25;
+var greeleyAge = new Date().getFullYear() - 1988;
+var mcLovinAge = new Date().getFullYear() - 1996;
 
-if (age < 21) {
-	console.log('Get outta my bar!')
+if (mcLovinAge < 21) {
+	console.log('Get outta my bar!');
 }
 else {
-	'What can I get you to drink?'
+	console.log('What can I get you to drink?');
 }
